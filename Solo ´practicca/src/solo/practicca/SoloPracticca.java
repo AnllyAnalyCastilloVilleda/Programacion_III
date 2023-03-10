f4eac330da29d2d0f9428c2491056d50d85e6d77
Anlly Castillo <110704559+AnllyAnalyCastilloVilleda@users.noreply.github.com>
2023-03-10 16:20:42 -0600

    Create SoloPracticca.java
    es solo por estudiar

---
+++ Solo ´practicca/src/solo/practicca/SoloPracticca.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package solo.practicca;
+
+/**
+ *
+ * @author anlly
+ */
+public class SoloPracticca {
+
+    // estoy aburrida
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
